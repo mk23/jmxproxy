80ef7b00713a9f0c4dcd35f87bc1a49f5c54bebc
Max Kalika <max@topsy.com>
2011-11-11 08:52:16 -0800

    Refactor sample cacti script server library to allow for more code reuse

--- ss_jmxproxy.php
+++ ss_jmxproxy.php
@@ -2,18 +2,18 @@
 
 $no_http_headers = true;
 
-/* display no errors */
+/* display only fatal errors */
 error_reporting(E_ERROR);
 include_once(dirname(__FILE__) . "/../include/config.php");
 include_once(dirname(__FILE__) . "/../lib/snmp.php");
 
 if (!isset($called_by_script_server)) {
     error_reporting(E_ALL);
-    array_shift($_SERVER["argv"]);
-    echo call_user_func_array("ss_jmxproxy", $_SERVER["argv"]), "\n";
+    $self = array_shift(explode('.', array_shift($_SERVER["argv"])));
+    echo call_user_func_array($self, $_SERVER["argv"]), "\n";
 }
 
-function ss_jmxproxy($host, $jmxproxy = 'localhost:8080') {
+function ss_jmxproxy($host, $jmxproxy = 'localhost:8080', $extra_stats = array()) {
     $jvm_stats = array(
         'thread_count' => array('java.lang:type=Threading', 'ThreadCount'),
         'thread_peak' => array('java.lang:type=Threading', 'PeakThreadCount'),
@@ -25,20 +25,37 @@ function ss_jmxproxy($host, $jmxproxy = 'localhost:8080') {
         'classes_unloaded' => array('java.lang:type=ClassLoading', 'UnloadedClassCount'),
     );
 
-    preg_match_all('/<a href=.+?>(.+?)<\/a>/', file_get_contents("http://{$jmxproxy}/jmxproxy/{$host}/java.lang"), $mbeans);
-    $mbeans = array_pop($mbeans);
+    $stats = array_merge($jvm_stats, $extra_stats);
+    $beans = array();
+    foreach (array_unique(array_map(function($name) { return array_shift(explode(':', $name[0])); }, $stats)) as $domain) {
+        preg_match_all('/<a href=.+?>(.+?)<\/a>/', file_get_contents("http://{$jmxproxy}/jmxproxy/{$host}/{$domain}"), $found);
+        $beans = array_merge($beans, $found[1]);
+    }
 
     $data = array();
-    foreach ($jvm_stats as $key => $val) {
-        $attribute = json_decode(file_get_contents("http://{$jmxproxy}/jmxproxy/{$host}/{$val[0]}/{$val[1]}"));
-        if (is_object($attribute)) {
-            if (sizeof($val) == 3) {
-                $data[$key] = $attribute->$val[2];
+    foreach ($stats as $key => $val) {
+        if (is_null($val) || sizeof($val) < 2) {
+            continue;
+        }
+        foreach (array_filter($beans, function($name) use ($val) { return preg_match("/^{$val[0]}\$/", $name); }) as $mbean) {
+            $request = sprintf(
+                "http://%s/jmxproxy/%s/%s/%s", $jmxproxy,
+                rawurlencode($host), rawurlencode($mbean), rawurlencode($val[1])
+            );
+            $attribute = json_decode(file_get_contents($request));
+            if (is_object($attribute)) {
+                if (sizeof($val) == 3) {
+                    if (array_key_exists($key, $data)) {
+                        $data[$key] += $attribute->$val[2];
+                    } else {
+                        $data[$key] = $attribute->$val[2];
+                    }
+                }
+            } else {
+                $data[$key] = $attribute;
             }
-        } else {
-            $data[$key] = $attribute;
         }
     }
 
-    return implode(' ', array_map(function($k,$v) { return $k . ':' . (is_null($v) ? 'U' : $v); }, array_keys($data), array_values($data)));
+    return implode(' ', array_map(function($k,$v) { return $k . ':' . (is_numeric($v) ? $v : 'U'); }, array_keys($data), array_values($data)));
 }
