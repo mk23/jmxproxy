1d41901db863b06b9c7a74f6b5135a706545dc54
Max Kalika <max@topsy.com>
2013-02-20 19:31:00 -0800

    Implement dropwizard service.

---
+++ src/main/java/com/topsy/jmxproxy/JMXProxyConfiguration.java
@@ -0,0 +1,6 @@
+package com.topsy.jmxproxy;
+
+import com.yammer.dropwizard.config.Configuration;
+
+public class JMXProxyConfiguration extends Configuration {
+}
---
+++ src/main/java/com/topsy/jmxproxy/JMXProxyService.java
@@ -0,0 +1,33 @@
+package com.topsy.jmxproxy;
+
+import com.topsy.jmxproxy.jmx.ConnectionManager;
+import com.topsy.jmxproxy.JMXProxyResource;
+
+import com.yammer.dropwizard.Service;
+import com.yammer.dropwizard.config.Bootstrap;
+import com.yammer.dropwizard.config.Environment;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JMXProxyService extends Service<JMXProxyConfiguration> {
+    private static final Logger LOG = LoggerFactory.getLogger(JMXProxyService.class);
+
+    public static void main(String[] args) throws Exception {
+        LOG.info("starting jmxproxy service");
+        new JMXProxyService().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<JMXProxyConfiguration> bootstrap) {
+        bootstrap.setName("jmxproxy");
+    }
+
+    @Override
+    public void run(JMXProxyConfiguration configuration, Environment environment) {
+        final ConnectionManager manager = new ConnectionManager();
+
+        environment.manage(manager);
+        environment.addResource(new JMXProxyResource(manager));
+    }
+}
