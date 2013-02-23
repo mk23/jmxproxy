0df1ffd49077d9e86a15dabd5aa6670cea2d5cdd
Max Kalika <max@topsy.com>
2013-02-22 16:40:38 -0800

    Pass extra flags (i.e. --major/minor/patch, etc) to release script.

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 cd $(dirname $(dirname $(readlink -e $0)))
-exec scripts/release.py -e pom.xml '<artifactId>jmxproxy</artifactId>\s+<version>{version}</version>' --commit
+exec scripts/release.py -e pom.xml '<artifactId>jmxproxy</artifactId>\s+<version>{version}</version>' --commit $@
