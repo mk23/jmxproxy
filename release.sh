55069a030d4ea550ab223df671251059b078971b
Max Kalika <max@topsy.com>
2013-03-12 13:44:09 -0700

    Use sandbox release.py directly from github.

---
+++ release.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -e
+
+cd $(dirname $(readlink -e $0))
+/usr/bin/curl -s https://raw.github.com/mk23/sandbox/master/misc/release.py | exec /usr/bin/env python2.7 - --commit -e pom.xml '<artifactId>jmxproxy</artifactId>\s+<version>{version}</version>' $@
--- scripts/release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-cd $(dirname $(dirname $(readlink -e $0)))
-exec scripts/release.py -e pom.xml '<artifactId>jmxproxy</artifactId>\s+<version>{version}</version>' --commit $@
