4aa74be4ca75a7a5d424ff79b85f2aafc445bd75
Max Kalika <max@topsy.com>
2013-03-24 07:07:35 -0700

    Make the test a bit less idiotic.

--- src/test/java/com/topsy/jmxproxy/jmx/tests/ConnectionCredentialsTest.java
+++ src/test/java/com/topsy/jmxproxy/jmx/tests/ConnectionCredentialsTest.java
@@ -9,24 +9,24 @@ import static org.junit.Assert.assertFalse;
 public class ConnectionCredentialsTest {
     @Test
     public void compareEquals() throws Exception {
-        final ConnectionCredentials auth1 = new ConnectionCredentials("user", "pass");
-        final ConnectionCredentials auth2 = new ConnectionCredentials("user", "pass");
+        final ConnectionCredentials auth1 = new ConnectionCredentials(new String("user"), new String("pass"));
+        final ConnectionCredentials auth2 = new ConnectionCredentials(new String("user"), new String("pass"));
 
         assertTrue(auth1.equals(auth2));
     }
 
     @Test
     public void compareNotEquals() throws Exception {
-        final ConnectionCredentials auth1 = new ConnectionCredentials("user", "pass");
-        final ConnectionCredentials auth2 = new ConnectionCredentials("pass", "user");
+        final ConnectionCredentials auth1 = new ConnectionCredentials(new String("user"), new String("pass"));
+        final ConnectionCredentials auth2 = new ConnectionCredentials(new String("pass"), new String("user"));
 
         assertFalse(auth1.equals(auth2));
     }
 
     @Test
     public void compareNotEqualsNull() throws Exception {
-        final ConnectionCredentials auth1 = new ConnectionCredentials("user", "pass");
+        final ConnectionCredentials auth = new ConnectionCredentials(new String("user"), new String("pass"));
 
-        assertFalse(auth1.equals(null));
+        assertFalse(auth.equals(null));
     }
 }
