1a5114d7f16b1978e221e962732e13fa4010fb39
Max Kalika <max@topsy.com>
2013-03-23 18:20:45 -0700

    Issue #4: Add support class for username/password processing.

---
+++ src/main/java/com/topsy/jmxproxy/jmx/ConnectionCredentials.java
@@ -0,0 +1,28 @@
+package com.topsy.jmxproxy.jmx;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class ConnectionCredentials {
+    @NotEmpty
+    @JsonProperty
+    private final String username;
+
+    @NotEmpty
+    @JsonProperty
+    private final String password;
+
+    public ConnectionCredentials(@JsonProperty("username") String username, @JsonProperty("password") String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
