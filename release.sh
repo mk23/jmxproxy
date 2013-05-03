61500a17f86749be43f3408e4e0c7b83964efecf
Max Kalika <max@topsy.com>
2013-05-03 15:03:06 -0700

    Automatically commit changelog and tag when running release.sh.

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 cd $(dirname $(readlink -e $0))
-/usr/bin/curl -s https://raw.github.com/mk23/sandbox/master/misc/release.py | exec /usr/bin/env python2.7 - -e pom.xml '<artifactId>jmxproxy</artifactId>\s+<version>{version}</version>' -e README.md 'target/jmxproxy-{version}.jar' -e scripts/server/start.sh 'target/jmxproxy-{version}.jar' $@
+/usr/bin/curl -s https://raw.github.com/mk23/sandbox/master/misc/release.py | exec /usr/bin/env python2.7 - --commit -e pom.xml '<artifactId>jmxproxy</artifactId>\s+<version>{version}</version>' -e README.md 'target/jmxproxy-{version}.jar' -e scripts/server/start.sh 'target/jmxproxy-{version}.jar' $@
