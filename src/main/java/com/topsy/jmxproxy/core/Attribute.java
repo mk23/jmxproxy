0c2173e2f12f5d972c62a7e4910ad8603253d9a5
Max Kalika <max@topsy.com>
2013-07-19 18:29:25 -0700

    Issue #22: Return json for strings that look like json.

--- src/main/java/com/topsy/jmxproxy/core/Attribute.java
+++ src/main/java/com/topsy/jmxproxy/core/Attribute.java
@@ -1,14 +1,23 @@
 package com.topsy.jmxproxy.core;
 
+import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.JsonSerializable;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
 
 import java.io.IOException;
 
 import java.lang.reflect.Array;
+
+import java.util.List;
+import java.util.ArrayList;
 
 import javax.management.openmbean.CompositeData;
 import javax.management.openmbean.TabularData;
@@ -36,6 +45,10 @@ public class Attribute implements JsonSerializable {
     private void buildJson(JsonGenerator jgen, Object objectValue) throws IOException, JsonProcessingException {
         if (objectValue == null) {
             jgen.writeNull();
+        } else if (objectValue instanceof Boolean) {
+            jgen.writeBoolean((Boolean)objectValue);
+        } else if (objectValue instanceof JsonNode) {
+            jgen.writeTree((JsonNode)objectValue);
         } else if (objectValue.getClass().isArray()) {
             jgen.writeStartArray();
             int length = Array.getLength(objectValue);
@@ -74,10 +87,32 @@ public class Attribute implements JsonSerializable {
             } else {
                 jgen.writeNumber(((Number)objectValue).toString());
             }
-        } else if (objectValue instanceof Boolean) {
-            jgen.writeBoolean((Boolean)objectValue);
         } else {
-            jgen.writeString(objectValue.toString());
+            try {
+                String input = objectValue.toString();
+                List<JsonNode> parts = new ArrayList<JsonNode>();
+                ObjectMapper mapper = new ObjectMapper();
+                JsonParser parser = new JsonFactory().createJsonParser(input);
+
+                for (parser.nextToken(); parser.hasCurrentToken(); parser.nextToken()) {
+                    JsonToken tok = parser.getCurrentToken();
+                    int pos = (int)parser.getTokenLocation().getCharOffset();
+                    if (tok == JsonToken.START_OBJECT || tok == JsonToken.START_ARRAY) {
+                        parser.skipChildren();
+                    }
+                    parts.add(mapper.readTree(input.substring(pos, (int)parser.getTokenLocation().getCharOffset() + parser.getTextLength() + (tok == JsonToken.VALUE_STRING ? 2 : 0))));
+                }
+
+                if (parts.isEmpty()) {
+                    jgen.writeString("");
+                } else if (parts.size() == 1) {
+                    buildJson(jgen, parts.get(0));
+                } else {
+                    buildJson(jgen, parts);
+                }
+            } catch (JsonParseException e) {
+                jgen.writeString(objectValue.toString());
+            }
         }
     }
 }
