6716f897cb17a95a91ba70eb6bfb31b41a768845
Max Kalika <mkalika@apple.com>
2014-11-22 12:42:00 -0800

    Issue #33: Detect branches and offer to push upstream when releasing.  Also be resilient to release script being unavailable.

--- release.sh
+++ release.sh
@@ -1,4 +1,36 @@
 #!/bin/bash -e
 
-cd $(dirname $(readlink -e $0))
-/usr/bin/curl -s https://raw.github.com/mk23/sandbox/master/misc/release.py | exec /usr/bin/env python2.7 - --commit -e pom.xml '<artifactId>jmxproxy</artifactId>\s+<version>{version}</version>' -e README.md 'jmxproxy-{version}.jar' -e scripts/server/start.sh 'jmxproxy-{version}.jar' $@
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [ "${BRANCH}" = "HEAD" ] ; then
+	echo "Cannot release on detached HEAD.  Please switch to a branch."
+	exit 1
+fi
+
+read -p "Would you like to commit ${BRANCH} branch? [y/N] " -r
+if [[ $REPLY =~ ^[Yy]$ ]] ; then
+	COMMIT=--commit
+fi
+
+echo
+(
+	/usr/bin/curl -L -s https://raw.github.com/mk23/sandbox/master/misc/release.py ||
+	echo 'raise Exception("unable to load release.py")'
+) |
+	exec /usr/bin/env python2.7 - ${COMMIT} --release stable \
+		-e pom.xml '<artifactId>jmxproxy</artifactId>\s+<version>{version}</version>' \
+		-e README.md 'jmxproxy-{version}.jar' \
+		-e scripts/server/start.sh 'jmxproxy-{version}.jar' \
+		"$@"
+
+if [ -n "${COMMIT}" ] ; then
+	echo
+	TAG=$(git describe --abbrev=0 --tags)
+	echo "Created tag ${TAG}"
+
+	for REMOTE in $(git remote -v | cut -f1 | uniq) ; do
+		read -p "Would you like to push to ${REMOTE}? [y/N] " -r
+		if [[ $REPLY =~ ^[Yy]$ ]] ; then
+		    git push "${REMOTE}" "${BRANCH}" "${TAG}"
+		fi
+	done
+fi
