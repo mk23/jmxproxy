be89568d7ed640b490fb065582d4a54028edb93a
Max Kalika <max.kalika+projects@gmail.com>
2015-05-10 00:41:11 -0700

    Issue #57: Add request query param for attribute history.

--- src/main/java/com/topsy/jmxproxy/JMXProxyResource.java
+++ src/main/java/com/topsy/jmxproxy/JMXProxyResource.java
@@ -46,26 +46,26 @@ public class JMXProxyResource {
 
     @GET
     @Path("{host}:{port:\\d+}/{mbean}/{attribute}")
-    public Response getJMXHostData(@PathParam("host") String host, @PathParam("port") int port, @PathParam("mbean") String mbean, @PathParam("attribute") String attribute) {
+    public Response getJMXHostData(@PathParam("host") String host, @PathParam("port") int port, @PathParam("mbean") String mbean, @PathParam("attribute") String attribute, @QueryParam("history") @DefaultValue("-1") int history) {
         LOG.debug("fetching jmx data for " + host + ":" + port + "/" + mbean + "/" + attribute);
-        return getJMXHost(host, port, mbean, attribute, null);
+        return getJMXHost(host, port, mbean, attribute, history, null);
     }
 
     @POST
     @Path("{host}:{port:\\d+}/{mbean}/{attribute}")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public Response getJMXHostData(@PathParam("host") String host, @PathParam("port") int port, @PathParam("mbean") String mbean, @PathParam("attribute") String attribute, @FormParam("username") String username, @FormParam("password") String password) {
+    public Response getJMXHostData(@PathParam("host") String host, @PathParam("port") int port, @PathParam("mbean") String mbean, @PathParam("attribute") String attribute, @QueryParam("history") @DefaultValue("-1") int history, @FormParam("username") String username, @FormParam("password") String password) {
         LOG.debug("fetching jmx data for " + username + "@" + host + ":" + port + "/" + mbean + "/" + attribute);
-        return getJMXHost(host, port, mbean, attribute, new ConnectionCredentials(username, password));
+        return getJMXHost(host, port, mbean, attribute, history, new ConnectionCredentials(username, password));
     }
 
     @POST
     @Path("{host}:{port:\\d+}/{mbean}/{attribute}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response getJMXHostData(@PathParam("host") String host, @PathParam("port") int port, @PathParam("mbean") String mbean, @PathParam("attribute") String attribute, @Valid ConnectionCredentials auth) {
+    public Response getJMXHostData(@PathParam("host") String host, @PathParam("port") int port, @PathParam("mbean") String mbean, @PathParam("attribute") String attribute, @QueryParam("history") @DefaultValue("-1") int history, @Valid ConnectionCredentials auth) {
         LOG.debug("fetching jmx data for " + auth.getUsername() + "@" + host + ":" + port + "/" + mbean + "/" + attribute);
 
-        return getJMXHost(host, port, mbean, attribute, auth);
+        return getJMXHost(host, port, mbean, attribute, history, auth);
     }
 
     @GET
@@ -78,7 +78,7 @@ public class JMXProxyResource {
     @POST
     @Path("{host}:{port:\\d+}/{mbean}")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public Response getJMXHostData(@PathParam("host") String host, @PathParam("port") int port, @PathParam("mbean") String mbean, @FormParam("username") String username, @FormParam("password") String password, @QueryParam("full") @DefaultValue("false") BooleanParam full) {
+    public Response getJMXHostData(@PathParam("host") String host, @PathParam("port") int port, @PathParam("mbean") String mbean, @QueryParam("full") @DefaultValue("false") BooleanParam full, @FormParam("username") String username, @FormParam("password") String password) {
         LOG.debug("fetching jmx data for " + username + "@" + host + ":" + port + "/" + mbean + " (full:" + full.get() + ")");
         return getJMXHost(host, port, mbean, full.get(), new ConnectionCredentials(username, password));
     }
@@ -102,7 +102,7 @@ public class JMXProxyResource {
     @POST
     @Path("{host}:{port:\\d+}")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public Response getJMXHostData(@PathParam("host") String host, @PathParam("port") int port, @FormParam("username") String username, @FormParam("password") String password, @QueryParam("full") @DefaultValue("false") BooleanParam full) {
+    public Response getJMXHostData(@PathParam("host") String host, @PathParam("port") int port, @QueryParam("full") @DefaultValue("false") BooleanParam full, @FormParam("username") String username, @FormParam("password") String password) {
         LOG.debug("fetching jmx data for " + username + "@" + host + ":" + port + " (full:" + full.get() + ")");
         return getJMXHost(host, port, full.get(), new ConnectionCredentials(username, password));
     }
@@ -112,12 +112,12 @@ public class JMXProxyResource {
     @Consumes(MediaType.APPLICATION_JSON)
     public Response getJMXHostData(@PathParam("host") String host, @PathParam("port") int port, @QueryParam("full") @DefaultValue("false") BooleanParam full, @Valid ConnectionCredentials auth) {
         LOG.debug("fetching jmx data for " + auth.getUsername() + "@" + host + ":" + port + " (full:" + full.get() + ")");
-
         return getJMXHost(host, port, full.get(), auth);
     }
 
     private Response getJMXHost(String hostName, int port, boolean full, ConnectionCredentials auth) {
-        Host host = manager.getHost(hostName + ":" + port, auth); if (host == null) {
+        Host host = manager.getHost(hostName + ":" + port, auth);
+        if (host == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
 
@@ -138,7 +138,7 @@ public class JMXProxyResource {
         return Response.ok(full ? mbean : mbean.getAttributes()).build();
     }
 
-    private Response getJMXHost(String hostName, int port, String mbeanName, String attribute, ConnectionCredentials auth) {
+    private Response getJMXHost(String hostName, int port, String mbeanName, String attribute, int history, ConnectionCredentials auth) {
         Host host = manager.getHost(hostName + ":" + port, auth);
         if (host == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
@@ -149,6 +149,10 @@ public class JMXProxyResource {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
 
-        return Response.ok(mbean.getAttribute(attribute)).build();
+        if (history < 0) {
+            return Response.ok(mbean.getAttribute(attribute)).build();
+        } else {
+            return Response.ok(mbean.getAttributes(attribute, history != 0 ? history : Integer.MAX_VALUE)).build();
+        }
     }
 }
