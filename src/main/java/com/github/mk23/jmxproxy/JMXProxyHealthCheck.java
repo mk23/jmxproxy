d46f617e04c2876cb063abe4403b774f3874cafe
Max Kalika <max.kalika+projects@gmail.com>
2016-01-30 16:36:19 -0800

    Issue #75: Make JMXProxyHealthCheck.java checkstyle clean.

--- src/main/java/com/github/mk23/jmxproxy/JMXProxyHealthCheck.java
+++ src/main/java/com/github/mk23/jmxproxy/JMXProxyHealthCheck.java
@@ -4,15 +4,37 @@ import com.github.mk23.jmxproxy.jmx.ConnectionManager;
 
 import com.codahale.metrics.health.HealthCheck;
 
+/**
+ * <p>Service health check.</p>
+ *
+ * Checks the state of the {@link ConnectionManager}.
+ *
+ * @author  mk23
+ * @since   2015-05-11
+ * @version 3.2.0
+ */
 public class JMXProxyHealthCheck extends HealthCheck {
+    /**
+     * Lifecycle {@link ConnectionManager} object that determines
+     * the overall service status.
+     */
     private final ConnectionManager manager;
 
-    public JMXProxyHealthCheck(ConnectionManager manager) {
+    /**
+     * <p>Default constructor.</p>
+     *
+     * Uses the {@link ConnectionManager} object to determine
+     * service status when a health check is requested.
+     *
+     * @param manager of the endpoint cache.
+     */
+    public JMXProxyHealthCheck(final ConnectionManager manager) {
         this.manager = manager;
     }
 
+    /** {@inheritDoc} */
     @Override
-    protected Result check() throws Exception {
+    protected final Result check() throws Exception {
         if (!manager.isStarted()) {
             return Result.unhealthy("connection manager is not started");
         }
