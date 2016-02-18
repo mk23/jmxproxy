f0fe4bf1c78b3d4b2ec504f81cf7aee7a2a218da
Max Kalika <max.kalika+projects@gmail.com>
2016-02-18 10:29:35 -0800

    Issue #75: Make jmx/ConnectionCredentials.java checkstyle clean.

--- src/main/java/com/github/mk23/jmxproxy/jmx/ConnectionCredentials.java
+++ src/main/java/com/github/mk23/jmxproxy/jmx/ConnectionCredentials.java
@@ -4,6 +4,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import org.hibernate.validator.constraints.NotEmpty;
 
+/**
+ * <p>JMX Username and Password user input POJO.</p>
+ *
+ * Used to marshal JMX agent credentials and provide methods for comparing.
+ *
+ * @author  mk23
+ * @since   2015-05-11
+ * @version 3.2.0
+ */
 public class ConnectionCredentials {
     @NotEmpty
     @JsonProperty
@@ -15,22 +24,49 @@ public class ConnectionCredentials {
 
     private final String combined;
 
-    public ConnectionCredentials(@JsonProperty("username") String username, @JsonProperty("password") String password) {
+    /**
+     * <p>Default constructor.</p>
+     *
+     * Sets the instance username and password {@link java.lang.String}s from provided parameters.
+     * Builds a single combined credential string for the hashCode computation.
+     *
+     * @param username username {@link java.lang.String} for the new credential instance.
+     * @param password password {@link java.lang.String} for the new credential instance.
+     */
+    public ConnectionCredentials(
+        @JsonProperty("username") final String username,
+        @JsonProperty("password") final String password
+    ) {
         this.username = username;
         this.password = password;
         this.combined = username + "\0" + password;
     }
 
-    public String getUsername() {
+    /**
+     * <p>Getter for username.</p>
+     *
+     * Fetches the stored username {@link java.lang.String}.
+     *
+     * @return username string.
+     */
+    public final String getUsername() {
         return username;
     }
 
-    public String getPassword() {
+    /**
+     * <p>Getter for password.</p>
+     *
+     * Fetches the stored password {@link java.lang.String}.
+     *
+     * @return password string.
+     */
+    public final String getPassword() {
         return password;
     }
 
+    /** {@inheritDoc} */
     @Override
-    public boolean equals(Object peer) {
+    public final boolean equals(final Object peer) {
         if (peer == null || !(peer instanceof ConnectionCredentials)) {
             return false;
         }
@@ -39,8 +75,9 @@ public class ConnectionCredentials {
         return username.equals(auth.getUsername()) && password.equals(auth.getPassword());
     }
 
+    /** {@inheritDoc} */
     @Override
-    public int hashCode() {
+    public final int hashCode() {
         return combined.hashCode();
     }
 }
